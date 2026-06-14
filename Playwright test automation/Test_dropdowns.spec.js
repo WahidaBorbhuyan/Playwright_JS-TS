36eec6468e10fb85df8acbb17e7afceb7b5c87d6
wahida borbhuyan <wahida55@gmail.com>
2026-06-14 20:41:23 +0530

    Fixed errors

--- Playwright test automation/Test_dropdowns.spec.js
+++ Playwright test automation/Test_dropdowns.spec.js
@@ -2,31 +2,35 @@ import {test,expect} from '@playwright/test';
 test('Dropdowns', async ({page}) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
     await page.locator("#country").selectOption("India");
-    await expect(page.locator("#country")).toHaveValue("India");
+    await expect(page.locator("#country")).toHaveValue("india");
     // await page.locator("#country").selectOption({label :"USA"});
     // await expect(page.locator("#country")).toHaveValue("USA");
     // await page.locator("#country").selectOption({value :"germany"});
     // await expect(page.locator("#country")).toHaveValue("germany");
     // await page.locator("#country").selectOption({index : 1});
+    await page.waitForTimeout(2000);
 })
 
 test('Dropdown options',async ({page})  =>{
     await page.goto("https://testautomationpractice.blogspot.com/");
     const countries = await page.locator("#country option")
     await expect(countries).toHaveCount(10);
+    await page.waitForTimeout(2000);
 })
 
-test('Dropdown options',async ({page})  =>{
+test('Dropdown options array',async ({page})  =>{
     await page.goto("https://testautomationpractice.blogspot.com/");
     const countries = await page.$$("#country option")
     countryLength = countries.length;
     await expect(countryLength).toBe(10);
+    await page.waitForTimeout(2000);
 })
 
-test('Prensence in dropdown',async ({page})  =>{
+test('Presence in dropdown',async ({page})  =>{
     await page.goto("https://testautomationpractice.blogspot.com/");
-    const countries = await page("#country").textContent();
-    await expect(content.includes("India")).toBeTruthy();
+    const countries = await page.locator("#country").textContent();
+    await expect(countries.includes("India")).toBeTruthy();
+    await page.waitForTimeout(2000);
 })
 
 test('Loop through values',async ({page})  =>{
@@ -35,10 +39,12 @@ test('Loop through values',async ({page})  =>{
     const countries = await page.$$("#country option");
     for (const country of countries){
         const countryName = await country.textContent();
-        if (countryName === "India"){
+        if (countryName.includes("France")){
             status = true;
             break;
         }
     }
     await expect(status).toBeTruthy();
+    await page.waitForTimeout(2000);
 })
+
